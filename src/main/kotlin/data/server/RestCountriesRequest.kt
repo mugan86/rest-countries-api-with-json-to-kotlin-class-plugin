0ac9501c5866906b8b27faf51084baaffc997dde
Anartz Mugika <mugan86@gmail.com>
2017-10-12 21:14:10 +0200

    Remove companion object and import to ConstantValues file

--- src/main/kotlin/data/server/RestCountriesRequest.kt
+++ src/main/kotlin/data/server/RestCountriesRequest.kt
@@ -4,15 +4,13 @@ import com.google.gson.Gson
 import domain.models.RestCountries
 import java.net.URL
 import com.google.gson.reflect.TypeToken
+import data.ConstantValues
 
 /******************************************************************************************************************
  * Created by Anartz Mugika (mugan86@gmail.com) on 04/10/2017.
  * Take data from server
  ******************************************************************************************************************/
 class RestCountriesRequest() {
-    companion object {
-        private val URL_LOCALHOST = "https://restcountries.eu/rest/v2/all"
-    }
 
-    fun execute() : List<RestCountries> = Gson().fromJson(URL(URL_LOCALHOST).readText(), object : TypeToken<List<RestCountries>>() {}.type)
+    fun execute() : List<RestCountries> = Gson().fromJson(URL(ConstantValues.Companion.URL_LOCALHOST).readText(), object : TypeToken<List<RestCountries>>() {}.type)
 }
