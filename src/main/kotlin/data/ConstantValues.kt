0b9e6d67d073dc503198494af3f1727cf01aaa45
Anartz Mugika <mugan86@gmail.com>
2017-10-12 21:14:32 +0200

    Add constant values to use in module

---
+++ src/main/kotlin/data/ConstantValues.kt
@@ -0,0 +1,22 @@
+package data
+
+/**
+ * Created by anartzmugika on 12/10/17.
+ */
+class ConstantValues() {
+    companion object {
+        val URL_LOCALHOST = "https://restcountries.eu/rest/v2/all"
+        //DATABASE Values
+        private val DB_NAME = "restcountries"
+        val URL_DB = "jdbc:mysql://localhost:3306/" + DB_NAME
+        val USER = "root"
+        val PASS = ""
+
+        val ALL_COUNTRIES = "SELECT * FROM `rescountries_country`"
+
+        val insertValue = "INSERT IGNORE INTO `rescountries_country` (`cioc`, `name`, `topLevelDoimain`, `alpha2Code`, `alpha3Code`, `callingCodes`, `capital`, `altSpellings`, `region`, `subregion`, `population`, `lat`, `lng`, `demonym`, `area`, `gini`, `timezones`, `borders`, `nativeName`, `numericCode`, `flag`) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+
+    }
+
+}
