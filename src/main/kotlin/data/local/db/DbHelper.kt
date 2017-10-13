4078910822e0b89cf9bd5e68698ea7201f20f310
Anartz Mugika <mugan86@gmail.com>
2017-10-13 13:07:57 +0200

    Add dinamically insert and all countries insert

--- src/main/kotlin/data/local/db/DbHelper.kt
+++ src/main/kotlin/data/local/db/DbHelper.kt
@@ -4,25 +4,56 @@ import java.sql.Statement
 import java.sql.Connection
 import java.sql.ResultSet
 
-/**
- * Created by anartzmugika on 13/10/17.
- */
+/***********************************************************************************************************************
+ * Created by Anartz Mugika (mugan86@gmail.com) on 13/10/17.
+ * Manage Local DB operations
+ ***********************************************************************************************************************/
 class DbHelper {
     private var connection: Connection?
-    private var statement: Statement
+
     init {
         connection = Connection().connectToDB()
-        statement = connection?.createStatement() as Statement
     }
 
-    fun executeQuery(sql: String): ResultSet = this.statement.executeQuery(sql)
+    fun executeQuery(sql: String): ResultSet = (connection?.createStatement() as Statement).executeQuery(sql)
 
-    fun executeInsertUpdateOperation() {
+    fun executeInsertUpdateOperation(sql: String, listParameters : ArrayList<String>) {
+        val statement = connection?.prepareStatement(sql)
+        var i = 0
+        listParameters.map {
+            statement?.setString((i + 1), listParameters.get(i))
+            i++
+        }
+        statement?.update()
+    }
 
+    fun prepareAndExecuteQueryAllCountries(sql: String, listParameters : ArrayList<String>) {
+        val statement = connection?.prepareStatement(sql)
+        statement?.setString(1, listParameters.get(0))
+        statement?.setString(2, listParameters.get(1))
+        statement?.setString(3, listParameters.get(2))
+        statement?.setString(4, listParameters.get(3))
+        statement?.setString(5, listParameters.get(4))
+        statement?.setString(6, listParameters.get(5))
+        statement?.setString(7, listParameters.get(6))
+        statement?.setString(8, listParameters.get(7))
+        statement?.setString(9, listParameters.get(8))
+        statement?.setString(10, listParameters.get(9))
+        statement?.setInt(11, listParameters.get(10).toInt())
+        statement?.setString(12, listParameters.get(11))
+        statement?.setString(13, listParameters.get(12))
+        statement?.setString(14, listParameters.get(13))
+        statement?.setString(15, listParameters.get(14))
+        statement?.setString(16, listParameters.get(15))
+        statement?.setString(17, listParameters.get(16))
+        statement?.setString(18, listParameters.get(17))
+        statement?.setString(19, listParameters.get(18))
+        statement?.setString(20, listParameters.get(19))
+        statement?.setString(21, listParameters.get(20))
+        statement?.update()
     }
 
     fun close() {
         connection?.close()
-        statement.close()
     }
 }
